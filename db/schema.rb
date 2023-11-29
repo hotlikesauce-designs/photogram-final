74122efbb3c80cd1f5b67eca9f09e723d47f1eea
hotlikesauce-designs <146861584+hotlikesauce-designs@users.noreply.github.com>
2023-11-29 17:20:31 +0000

    created comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_29_171752) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_29_171923) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "author_id"
+    t.integer "photo_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "follow_requests", force: :cascade do |t|
     t.integer "sender_id"
     t.integer "recipient_id"
